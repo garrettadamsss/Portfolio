371f9c372b40b553b4990a96fcc9d105e72d9b6b
Garrett Adams <garrettadamssss@gmail.com>
2026-07-23 00:23:18 -0700

    fix nav links

--- src/app/component/navbar.tsx
+++ src/app/component/navbar.tsx
@@ -1,6 +1,6 @@
 const navLinks = [
-  { label: "Home", href: "#home" },
   { label: "About", href: "#about" },
+  { label: "Experience", href: "#experience" },
   { label: "Projects", href: "#projects" },
 ];
 
