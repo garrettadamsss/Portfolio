46487ff3b5267fa8253c1980da368b1487877ac7
Garrett Adams <garrettadamssss@gmail.com>
2026-08-05 00:18:43 -0700

    widen page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import ExperienceSection from "./component/experience";
 export default function Main() {
   return (
     <div className="min-h-screen text-foreground">
-      <main className="mx-auto w-full max-w-7xl pb-24 pt-10 ">
+      <main className="mx-auto max-w-screen-2xl px-6 py-12">
         <Navbar />
         <HomeSection />
         <AboutSection />
