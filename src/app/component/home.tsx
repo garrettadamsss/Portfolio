737131081d7d357cec71c09a20cef8c2174f997e
Adams <garrettadamssss@gmail.com>
2026-09-24 19:36:13 -0700

    change colors

--- src/app/component/home.tsx
+++ src/app/component/home.tsx
@@ -30,7 +30,7 @@ export default function HomeSection() {
             title="Github"
             className="transition hover:text-white"
           >
-            <svg viewBox="0 0 48 48" fill="currentColor" className="h-7 w-7">
+            <svg viewBox="0 0 48 48" fill="currentColor" className="icon">
               <path d="M24,1.9a21.6,21.6,0,0,0-6.8,42.2c1,.2,1.8-.9,1.8-1.8V39.4c-6,1.3-7.9-2.9-7.9-2.9a6.5,6.5,0,0,0-2.2-3.2C6.9,31.9,9,32,9,32a4.3,4.3,0,0,1,3.3,2c1.7,2.9,5.5,2.6,6.7,2.1a5.4,5.4,0,0,1,.5-2.9C12.7,32,9,28,9,22.6A10.7,10.7,0,0,1,11.9,15a6.2,6.2,0,0,1,.3-6.4,8.9,8.9,0,0,1,6.4,2.9,15.1,15.1,0,0,1,5.4-.8,17.1,17.1,0,0,1,5.4.7,9,9,0,0,1,6.4-2.8,6.5,6.5,0,0,1,.4,6.4A10.7,10.7,0,0,1,39,22.6C39,28,35.3,32,28.5,33.2a5.4,5.4,0,0,1,.5,2.9v6.2a1.8,1.8,0,0,0,1.9,1.8A21.7,21.7,0,0,0,24,1.9Z" />
             </svg>
           </a>
@@ -43,7 +43,7 @@ export default function HomeSection() {
             title="LinkedIn"
             className="transition hover:text-white"
           >
-            <svg viewBox="0 0 24 24" fill="currentColor" className="h-7 w-7">
+            <svg viewBox="0 0 24 24" fill="currentColor" className="icon">
               <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
             </svg>
           </a>
@@ -56,7 +56,7 @@ export default function HomeSection() {
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-7 w-7"
+              className="icon"
               viewBox="2 2 20 20"
               fill="none"
               stroke="currentColor"
@@ -91,7 +91,7 @@ export default function HomeSection() {
               fill="none"
               stroke="currentColor"
               strokeWidth="2"
-              className="h-7 w-7"
+              className="icon"
               aria-hidden="true"
             >
               <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
